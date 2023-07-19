611abf8748c7d4f68739e8fd440db8c0d91e18b4
Christopher Reid <creid@imdpath.com>
2023-07-18 18:13:03 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace goldisnotdead.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
