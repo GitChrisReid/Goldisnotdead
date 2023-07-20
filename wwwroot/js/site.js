5977ee95ffac8c142e659911a26bd307d197c6ba
Christopher Reid <creid@imdpath.com>
2023-07-20 13:19:54 -0700

    added typing javascript

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿window.onload = function () {
+    var i = 0;
+    var text = "Chris Reid";
+    var speed = 100; // You can adjust this value for typing speed
+    var container = document.getElementById('typed-text');
 
-// Write your JavaScript code.
+    function typeWriter() {
+        if (i < text.length) {
+            container.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        } else {
+            setTimeout(clearText, 7000); // Start clearText function after 7 seconds
+        }
+    }
+
+    function clearText() {
+        container.innerHTML = "";
+        i = 0;
+        typeWriter();
+    }
+
+    typeWriter(); // Start the typing animation
+}
