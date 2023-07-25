b7e90493de8ff97a971be70becd74f3c93decdad
Christopher Reid <creid@imdpath.com>
2023-07-24 18:53:57 -0700

    Fixed bug (and also stripped most of my css because it's garbagiooooo)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace Goldisnotdead.Controllers
         {
             return View();
         }
+        public IActionResult Projects()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
